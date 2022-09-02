4fbeb7334570f60fe1151ed702980074242dc164
CrysD5 <matrim.ivy@gmail.com>
2022-09-02 08:35:35 -0400

    studentDDL working partially

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,20 +19,34 @@ namespace TelerikMvcApp1.Controllers
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             var classYear = regEnt.vw_Matri_Grad_Dates
-                .Where(x => x.exp_grad_year != null)                
-                .Select(x => new { Year = x.exp_grad_year })
+                .Where(c => c.campuscode == "cwru")
+                .Where(c => c.Prog_status == "AC" || c.Prog_status == "LA")
+                .GroupBy(c => c.gyr)
+                .Select(c => c.FirstOrDefault())
+                .Select(c => new { classYear = c.gyr})
                 .Distinct()
-                .OrderByDescending(x => x.Year)
+                .Take(6)
+                .OrderByDescending(c => c.classYear)
                 .ToList();
+            classYear.Add(new {classYear = "LOA" });
+            classYear.Add(new { classYear = "PHD" });
+            ViewData["classYear"] = classYear;
+
+            return View();
+            //    .Where(x => x.exp_grad_year != null)                
+            //    .Select(x => new { Year = x.exp_grad_year })
+            //    .Distinct()
+            //    .OrderByDescending(x => x.Year)
+            //    .ToList();
             //classYear.Add(new { Year = "LOA" });
             //classYear.Add(new { Year = "PHD" });
-            ViewData["classYear"] = classYear;
-            return View();
+            //ViewData["classYear"] = classYear;
+            //return View();
         }
 
         public JsonResult GetSocDDL()
         {
-            var societies = regEnt.vw_Matri_Grad_Dates
+            var society = regEnt.vw_Matri_Grad_Dates
                 .Where(a => a.campuscode == "cwru")
                 .Where(a => a.Prog_status == "AC" || a.Prog_status == "LA")
                 .GroupBy(a => a.advisor_society)
@@ -41,77 +55,124 @@ namespace TelerikMvcApp1.Controllers
                 .Take(6)
                 .Distinct()       
                 .ToList();
-            societies.Add(new { Society = "All" });
+            society.Add(new { Society = "All" });
+            ViewData["society"] = society;
             //returns list of societies
-            return Json(societies, JsonRequestBehavior.AllowGet);
+            return Json(society, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetGyrDDL()
-        {
-            var gradYears = regEnt.vw_Matri_Grad_Dates
-                .Where(c => c.campuscode == "cwru")
-                .Where(c => c.Prog_status == "AC" || c.Prog_status == "LA")
-                //.Where(c => c.exp_grad_year == )
-                .GroupBy(c => c.gyr)
-                .Select(c => c.FirstOrDefault())
-                .Select(c => new { GradYear = c.gyr })
-                .Distinct()
-                .Take(6)
-                .OrderByDescending(c => c.GradYear)
-                .ToList();
-
-            gradYears.Add(new { GradYear = "LOA" });
-            gradYears.Add(new { GradYear = "PHD" });
-
-            return Json(gradYears, JsonRequestBehavior.AllowGet);
-
-        }
-
-        //public static List<StudentsModel> GetStudents()
+        //public JsonResult GetGyrDDL()
         //{
-        //   List<StudentsModel> students = from s in regEnt.Personals join si in regEnt.vw_Matri_Grad_Dates on 
-        //}
-        //public JsonResult GetStudentsDDL()
-        //{
-        //    var students = (from s in regEnt.Personals orderby s.Last_Name select s).ToList();
+        //    var gradYears = regEnt.vw_Matri_Grad_Dates
+        //        .Where(c => c.campuscode == "cwru")
+        //        .Where(c => c.Prog_status == "AC" || c.Prog_status == "LA")
+        //        //.Where(c => c.exp_grad_year == )
+        //        .GroupBy(c => c.gyr)
+        //        .Select(c => c.FirstOrDefault())
+        //        .Select(c => new { GradYear = c.gyr })
+        //        .Distinct()
+        //        .Take(6)
+        //        .OrderByDescending(c => c.GradYear)
+        //        .ToList();
 
-        //    return Json(students, JsonRequestBehavior.AllowGet);
+        //    gradYears.Add(new { GradYear = "LOA" });
+        //    gradYears.Add(new { GradYear = "PHD" });
+
+        //    return Json(gradYears, JsonRequestBehavior.AllowGet);
+
         //}
+
+       
         public string SaveSociety(string society)
         {
             Session["society"] = society;
 
             return society;
         }
-        public int? SaveGradYear(int classYear)
+        public string SaveGradYear(string classYear)
         {
             Session["gradYear"] = classYear;
 
             return classYear;
         }
 
-        public JsonResult CascadingGetStudents()
+        public JsonResult CascadingGetStudents(string classYear)
         {
             //if (Session["gradYear"] != null)
            string gradYear = string.Empty;
-            gradYear = Convert.ToString(Session["gradYear"]);
+            gradYear = Session["gradYear"].ToString();
 
-            var gradsByYear = regEnt.vw_Matri_Grad_Dates
-                .Where(x => x.exp_grad_year == gradYear)
-                .OrderBy(x => x.Emplid)
-                .Select(x => x.Emplid)
-                .ToList();
+            //if (gradYear.Contains("LOA") || gradYear.Contains("PHD"))
+            //{
+            //    var leaveStatus = regEnt.vw_Matri_Grad_Dates.Where(x => x.Prog_status == "LA").Select(x => x.Emplid).ToList();
 
-            var gradsInYear = gradsByYear.Select(x => Int32.Parse(x)).ToList();
+            //    var covertIdToInt = leaveStatus.Select(x => Int32.Parse(x)).ToList();
 
-            var personalInfo = regEnt.Personals
+            //    var onLeaveList = regEnt.Personals
+            //        .Where(x => leaveStatus.Contains(x.Emplid))
+            //        .OrderBy(x => x.Last_Name)
+            //        .Select(x => new { Name = x.Last_Name + ", " + x.First_Name })
+            //        .ToList();
+
+            //    return Json(onLeaveList, JsonRequestBehavior.AllowGet);
+            //}
+            
+                var gradsByYear = regEnt.vw_Matri_Grad_Dates
+               .Where(x => x.exp_grad_year == gradYear)
+               .OrderBy(x => x.Emplid)
+               .Select(x => x.Emplid)
+               .ToList();
+
+                var gradsInYear = gradsByYear.Select(x => Int32.Parse(x)).ToList();
+
+                var personalInfo = regEnt.Personals
                 .Where(x => gradsByYear.Contains(x.Emplid))
                 .OrderBy(x => x.Last_Name)
                 .Select(x => new { Name = x.Last_Name + ", " + x.First_Name })
-                .ToList();
+                .ToList();               
+
 
             return Json(personalInfo, JsonRequestBehavior.AllowGet);
         }
+
+        //public JsonResult GetTestDDL()
+        //{
+        //    //string society = string.Empty;
+        //    //society = (string)Session["society"];
+        //    //string gradYear = string.Empty;
+        //    //gradYear = Convert.ToString(Session["gradYear"]);
+
+        //    //string society = string.Empty;
+        //    //string gradYear = string.Empty;
+        //    //society = "Satcher";
+        //    //gradYear = "2024";
+        //    try
+        //    {
+        //        var gradsByYr = regEnt.vw_Matri_Grad_Dates
+        //            .Where(x => x.gyr == gradYear)
+        //            //.Where(x => x.Prog_status == "LA" || x.Prog_status == "DC")
+        //            .Where(x => x.advisor_society.Contains(society))
+        //            .OrderBy(x => x.Emplid)
+        //            .Select(x => x.Emplid)
+        //            .ToList();
+
+        //        var gradsInYear = gradsByYr.Select(x => Int32.Parse(x)).ToList();
+
+        //        var personalInfo = regEnt.Personals
+        //        .Where(x => gradsByYr.Contains(x.Emplid))
+        //        .OrderBy(x => x.Last_Name)
+        //        .Select(x => new { Name = x.Last_Name + ", " + x.First_Name })
+        //        .ToList();
+
+
+        //        return Json(personalInfo, JsonRequestBehavior.AllowGet);
+        //    }
+        //    catch 
+        //    {
+        //        //ErrorLogger.LogError(ex, "SocietyDeansMVC", System.Reflection.MethodInfo.GetCurrentMethod().Name, Request.UserAgent);
+        //        return Json("", JsonRequestBehavior.AllowGet);
+        //    }
+        //}
 
         public ActionResult ReportDetails()
         {
